24efd7e88b5760c3f1206bfcb434aca5ed0b6841
Colas Droin <colasdroin@gmail.com>
2023-05-02 18:06:39 +0200

    Update debug analysis

--- opt_flathv_75_1500_withBB_1p4_survival_chromas_debug/madx_000/xsuite_012/run.sh
+++ opt_flathv_75_1500_withBB_1p4_survival_chromas_debug/madx_000/xsuite_012/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /afs/cern.ch/work/c/cdroin/private/DA_study_for_bologna_cluster/miniconda/bin/activate
-cd /afs/cern.ch/work/c/cdroin/private/DA_study_for_bologna_cluster/opt_flathv_75_1500_withBB_1p4_survival_chromas_debug/madx_000/xsuite_012
+source /gpfs/gpfs/gpfs_maestro_home_new/hpc/cdroin/DA_study/miniconda/bin/activate
+cd /gpfs/gpfs/gpfs_maestro_home_new/hpc/cdroin/DA_study/opt_flathv_75_1500_withBB_1p4_survival_chromas_debug/madx_000/xsuite_012
 python 000_track.py > output.txt 2> error.txt
 rm -rf final_* modules optics_repository optics_toolkit tools tracking_tools temp mad_collider.log __pycache__ twiss* errors fc* optics_orbit_at*
