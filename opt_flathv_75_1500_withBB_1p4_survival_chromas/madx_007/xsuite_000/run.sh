1d6c20821c7ecc452cf00da0d27eafe505552bf4
Colas Droin <colasdroin@gmail.com>
2023-05-02 14:13:59 +0200

    Upload results

---
+++ opt_flathv_75_1500_withBB_1p4_survival_chromas/madx_007/xsuite_000/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /gpfs/gpfs/gpfs_maestro_home_new/hpc/cdroin/DA_study/miniconda/bin/activate
+cd /gpfs/gpfs/gpfs_maestro_home_new/hpc/cdroin/DA_study/opt_flathv_75_1500_withBB_1p4_survival_chromas/madx_007/xsuite_000
+python 000_track.py > output.txt 2> error.txt
+rm -rf final_* modules optics_repository optics_toolkit tools tracking_tools temp mad_collider.log __pycache__ twiss* errors fc* optics_orbit_at*
